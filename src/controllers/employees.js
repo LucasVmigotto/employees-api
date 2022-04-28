e175a54724743dccb1b26dc118ef0c85b3661c2a
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-04-28 13:17:05 -0300

    feat(employees): add employees controller base
    Create list employees function

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -1,18 +1,86 @@
-const EMPLOYEE_FIELDS = [
-  'id',
-  'name',
-  'email',
-  'department',
-  'salary',
-  'birth_date'
-]
+const {
+  EMPLOYEE_FIELDS,
+  makeResponse
+} = require('../utils')
 
-exports.listEmployees = () => {}
+exports.listEmployees = async ({ knex, logger }, res) => {
+  try {
 
-exports.getEmployee = () => {}
+    const [employees] = await knex('employees')
+      .select(EMPLOYEE_FIELDS)
 
-exports.createEmployee = () => {}
+    return makeResponse(
+      res,
+      200,
+      { employees }
+    )
 
-exports.updateEmployee = () => {}
+  } catch (err) {
+    logger.error(`Problem in listEmployees: ${err}`)
+    return makeResponse(
+      res,
+      500,
+      { error: 'An internal error occurred' }
+    )
+  }
+}
 
-exports.deleteEmployee = () => {}
+exports.getEmployee = async ({ knex, logger, params }, res) => {
+  try {
+
+    return
+
+  } catch (err) {
+    logger.error(`Problem in getEmployee: ${err}`)
+    return makeResponse(
+      res,
+      500,
+      { error: 'An internal error occurred' }
+    )
+  }
+}
+
+exports.createEmployee = async ({ knex, logger, body }, res) => {
+  try {
+
+    return
+
+  } catch (err) {
+    logger.error(`Problem in createEmployee: ${err}`)
+    return makeResponse(
+      res,
+      500,
+      { error: 'An internal error occurred' }
+    )
+  }
+}
+
+exports.updateEmployee = async ({ knex, logger, params, body }, res) => {
+  try {
+
+    return
+
+  } catch (err) {
+    logger.error(`Problem in updateEmployee: ${err}`)
+    return makeResponse(
+      res,
+      500,
+      { error: 'An internal error occurred' }
+    )
+  }
+}
+
+exports.deleteEmployee = async ({ knex, logger, params }, res) => {
+  try {
+
+    return
+
+  } catch (err) {
+    logger.error(`Problem in deleteEmployee: ${err}`)
+    return makeResponse(
+      res,
+      500,
+      { error: 'An internal error occurred' }
+    )
+  }
+}
