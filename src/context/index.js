bd1054fb4136b039917359595c0ed3361ec36f44
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-04-28 10:20:20 -0300

    feat(context): create project context folder
    Create knex file
    Create logger file
    Create main context file

---
+++ src/context/index.js
@@ -0,0 +1,13 @@
+const { createLogger } = require('./logger')
+const { createKnex } = require('./knex')
+
+exports.createContext = config => {
+  const logger = createLogger({ config })
+
+  const knex = createKnex({ config, logger })
+
+  return {
+    logger,
+    knex
+  }
+}
